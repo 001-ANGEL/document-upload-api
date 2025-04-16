196778f4f88589a1abbf6b9a8916d31bbfb3a620
Angel Lopez <angellopezixcayau@gmail.com>
2025-04-16 16:31:41 -0600

    feat(document): update DocumentModule and IDocument interface for improved user schema integration

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,20 +1,24 @@
 import { Module } from '@nestjs/common';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
-import { AuthValidation } from 'src/auth/providers/auth-validation.provider';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from 'src/user/schema/user.schema';
 import { FileSchema } from './schema/document.schema';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-      MongooseModule.forFeature([
-        { name: 'User', schema: UserSchema },
-        { name: 'File', schema: FileSchema },
-      ]),
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'File', schema: FileSchema },
+    ]),
+    AuthModule,
   ],
   controllers: [DocumentController],
-  providers: [DocumentService, AuthValidation],
-  exports: [DocumentService],
+  providers: [DocumentService],
+  exports: [
+    DocumentService,
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
 })
 export class DocumentModule {}
--- src/document/interface/document.interface.ts
+++ src/document/interface/document.interface.ts
@@ -1,10 +1,11 @@
 import { Document } from 'mongoose';
+import { UserSchema } from 'src/user/schema/user.schema';
 
 export interface IDocument extends Document {
     fileName: string;
     date: Date;
     extension: string;
     fileContent: string;
-    userId: any;
+    userId: typeof UserSchema; 
     isDeleted: boolean;
 }
